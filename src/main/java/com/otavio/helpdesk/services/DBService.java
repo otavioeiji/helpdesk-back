3ca8724a7239de885ec7cca28b882325221779ff
otavioeiji <otavioem@gmail.com>
2022-01-27 11:14:07 -0300

    Perfil de teste criado

---
+++ src/main/java/com/otavio/helpdesk/services/DBService.java
@@ -0,0 +1,43 @@
+package com.otavio.helpdesk.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.otavio.helpdesk.domain.Chamado;
+import com.otavio.helpdesk.domain.Cliente;
+import com.otavio.helpdesk.domain.Tecnico;
+import com.otavio.helpdesk.domain.enums.Perfil;
+import com.otavio.helpdesk.domain.enums.Prioridade;
+import com.otavio.helpdesk.domain.enums.Status;
+import com.otavio.helpdesk.repositories.ChamadoRepository;
+import com.otavio.helpdesk.repositories.ClienteRepository;
+import com.otavio.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	public void instanciaDB() {
+		
+		Tecnico tec1 = new Tecnico(null, "Valdir Cesar", "33134488361", "valdir@mail.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null, "Linus Torvalds", "36975935521", "torvalds@mail.com", "123");
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+		
+	}
+
+}

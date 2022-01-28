0186158b9586469f3680e11e90c09b1f44add25d
otavioeiji <otavioem@gmail.com>
2022-01-28 17:01:33 -0300

    Exceção para CPF e E-EMAIL já cadastrados

--- src/main/java/com/otavio/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/otavio/helpdesk/repositories/PessoaRepository.java
@@ -1,9 +1,13 @@
 package com.otavio.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.otavio.helpdesk.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
-
+	
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 }

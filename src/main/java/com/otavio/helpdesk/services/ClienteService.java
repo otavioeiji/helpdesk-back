edd88536f54eb6c124030b1ff051dbf7ec660376
Otávio Eiji Mitsuzaki <otavioem@gmail.com>
2022-02-04 10:18:19 -0300

    Update ClienteService.java

--- src/main/java/com/otavio/helpdesk/services/ClienteService.java
+++ src/main/java/com/otavio/helpdesk/services/ClienteService.java
@@ -57,7 +57,7 @@ public class ClienteService {
 		
 		if(obj.getChamados().size() > 0) {
 			throw new DataIntegrityViolationException("Cliente possui ordens de serviço e não pode ser deletado!");
-		} if(obj.getCpf() != "HOLE_ADMIN") {
+		} if(obj.getCpf() != "ADMIN") {
 			throw new DataIntegrityViolationException("Você não tem permissão para deletar!");
 		}
 		

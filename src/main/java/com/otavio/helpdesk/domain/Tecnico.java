f3bf15931571f567f51ac585fc5d0eca8a6cf78a
Otávio Eiji Mitsuzaki <otavioem@gmail.com>
2022-02-09 11:08:24 -0300

    Update Tecnico.java

--- src/main/java/com/otavio/helpdesk/domain/Tecnico.java
+++ src/main/java/com/otavio/helpdesk/domain/Tecnico.java
@@ -21,12 +21,12 @@ public class Tecnico extends Pessoa {
 
 	public Tecnico() {
 		super();
-		addPerfil(Perfil.CLIENTE);
+		addPerfil(Perfil.TECNICO);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
-		addPerfil(Perfil.CLIENTE);
+		addPerfil(Perfil.TECNICO);
 	}
 	
 	public Tecnico(TecnicoDTO obj) {

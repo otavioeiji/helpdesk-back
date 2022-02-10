61d32cd4882e15201d746166d35e5b1caf29445b
Otávio Eiji Mitsuzaki <otavioem@gmail.com>
2022-02-10 17:43:09 -0300

    Update TecnicoService.java

--- src/main/java/com/otavio/helpdesk/services/TecnicoService.java
+++ src/main/java/com/otavio/helpdesk/services/TecnicoService.java
@@ -66,7 +66,7 @@ public class TecnicoService {
 		
 		if(obj.getChamados().size() > 0) {
 			throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
-		} if(obj.getPerfis().toString() != "ROLE_ADMIN") {
+		} if(Perfil.TECNICO.getCodigo() != 0) {
 			throw new DataIntegrityViolationException("Não autorizado!");
 		}
 		repository.deleteById(id);
